8d5c20b39001903a0975c6f99eda0225a5220cd9
Soarezz <popcornor6topster@outlook.com>
2026-09-15 22:01:09 -0300

    Aula herança adicionado

---
+++ AulaHerança/src/br/com/controle/CalculoFilha1.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.controle;
+
+public class CalculoFilha1 extends Calculos{
+   public int somar(int valor1,int valor2){
+    return (valor1+valor2);
+   } 
+}
